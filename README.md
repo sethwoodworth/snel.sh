658cc688caa9e1116e7b00125de8fbf7f102fcbe
Seth Woodworth <seth@sethish.com>
2020-04-03 18:36:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🐌🐚 Snel shell
+
+A non-linear terminal interface.
+
+:ALPHA: This is an attempt at readme driven development.
+
+## Install
+
+    pip install --user snel
---
+++ snel/__main__.py
@@ -0,0 +1,7 @@
+from prompt_toolkit import print_formatted_text as pft, HTML
+
+def main():
+    pft(HTML('<ansired>helloworl</ansired><ansigreen>d</ansigreen>'))
+
+if __name__ == "__main__":
+    main()
